9cc841046ad8c4ddd35f2d2e633422ac727b2a0d
Prakritidev Verma <prakritidevverma@gmail.com>
2019-11-13 01:43:08 +0530

    updated folder structure

--- algorithms_practice_and_my_notes/linearSearch.py
+++
@@ -1,7 +0,0 @@
-'''
-Linear search is a linerar algo(statine the obvious). We go to each element and check if it's same as the query input or not.
-
-Time complexity if this algo is O(n), because we use a for loop and check each element, if n are number of element then loop will run n time.
-
-TADA! simple shit.
-'''
